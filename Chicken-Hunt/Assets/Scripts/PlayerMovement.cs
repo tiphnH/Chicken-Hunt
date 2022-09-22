cb603f3aad80497a708407b3bda3978510248f78
David <104822295+Dav-Chung@users.noreply.github.com>
2022-09-22 21:18:31 +0200

    G-Droite et jump

--- Chicken-Hunt/Assets/Scripts/PlayerMovement.cs
+++ Chicken-Hunt/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
  public float moveSpeed;
+ public float jumpForce;
+
+ private bool isJumping;
+ private bool isGrounded;
+public Transform groundCheckLeft;
+public Transform groundCheckRight;
 
  public Rigidbody2D rb;
  private Vector3 velocity = Vector3.zero;
@@ -14,14 +20,29 @@ public class PlayerMovement : MonoBehaviour
     
     void FixedUpdate()
     {
-        float horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-        
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
+
+               
+
         MovePlayer(horizontalMovement);
     }
 
-    void MovePlayer(float _horizontalMove)
+    void MovePlayer(float _horizontalMovement)
     {
-        Vector3 targetVelocity = new Vector2(_horizontalMove, rb.velocity.y);
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+        if(isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+            
+        }
     }
 }
